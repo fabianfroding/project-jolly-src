3b30316fb7c36452a765c4932da745729bbc07e0
fabian <fabian.froding@gmail.com>
2023-08-02 22:48:10 +0200

    fix player env dmg sequence

--- ENV/TakeEnvDamageOnCollisionEnter.cs
+++ ENV/TakeEnvDamageOnCollisionEnter.cs
@@ -4,12 +4,26 @@ public class TakeEnvDamageOnCollisionEnter : MonoBehaviour
 {
     [SerializeField] private Types.DamageData damageData;
 
+    private void Awake()
+    {
+        damageData.source = gameObject;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Entity damagedEntity = collision.gameObject.GetComponent<Entity>();
-        if (damagedEntity != null)
+        // Instantly kill enemies.
+        if (collision.gameObject.GetComponent<Enemy>())
         {
-            //damagedEntity.TakeDamage(damageData);
+            Death death = collision.gameObject.GetComponentInChildren<Death>();
+            if (death)
+            {
+                death.Die();
+                return;
+            }
         }
+
+        damageData.target = collision.gameObject;
+        IDamageable damageable = collision.gameObject.GetComponentInChildren<IDamageable>();
+        damageable?.TakeDamage(damageData);
     }
 }
