be1f58ab67a560a31878e2803b93c735788ae157
Fabian F <fabian.froding@gmail.com>
2024-06-26 21:52:50 +0200

    Update DamageHitBox.cs
     - Fixed a bug where the on trigger event wasnt enough to detect target hit by hitboxes.
     - Improved the logic surrounding damage hitbox.

--- Combat/DamageHitBox.cs
+++ Combat/DamageHitBox.cs
@@ -1,64 +1,78 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageHitBox : MonoBehaviour
 {
     [SerializeField] private Types.DamageData damageData;
+    [SerializeField] private LayerMask affectedLayerMask; // TODO: Consider moving this to damage data.
 
     private EnemyPawn owningEnemyPawn;
     private PlayerPawn owningPlayerPawn;
+
+    private Collider2D hitBoxCollider2D;
+    private List<Collider2D> cachedHitColliders; // Used to filter out already damaged targets.
 
     private void Awake()
     {
         owningEnemyPawn = GetComponentInParent<EnemyPawn>();
         owningPlayerPawn = GetComponentInParent<PlayerPawn>();
+        hitBoxCollider2D = GetComponent<Collider2D>();
+        cachedHitColliders = new();
     }
 
     private void OnEnable()
     {
+        DamageOverlappingTargets();
         GameFunctionLibrary.PlayRandomAudioAtPosition(damageData.audioClips, transform.position);
+    }
+
+    private void OnDisable()
+    {
+        cachedHitColliders.Clear();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Seems like this doesnt always trigger when enabling/disabling the hitbox.
-        IDamageable damageable = collision.GetComponentInChildren<IDamageable>();
-        if (damageable == null)
-        {
-            Debug.Log("DamageHitBox::OnTriggerEnter2D: Could not find IDamagable in children of target.");
+        if (IsFriendlyTarget(collision))
             return;
-        }
-
-        damageData.target = collision.gameObject;
-
-        if (owningPlayerPawn && collision.GetComponent<PlayerPawn>())
-        {
-            Debug.Log("DamageHitBox::OnTriggerEnter2D: Both owner and target is player.");
-            return;
-        }
-        if (owningEnemyPawn && collision.GetComponent<EnemyPawn>())
-        {
-            Debug.Log("DamageHitBox::OnTriggerEnter2D: Both owner and target is enemy.");
-            return;
-        }
-
-        damageable.TakeDamage(damageData);
+        DamageCollider(collision);
     }
 
-    // Code fix to use instead of on triger enter:
-
-    /*private void test()
+    private void DamageCollider(Collider2D collision)
     {
-        // Adjust the box center relative to the object's position if needed
-        Vector2 adjustedBoxCenter = (Vector2)transform.position + boxCenter;
+        if (cachedHitColliders.Contains(collision))
+            return;
+        IDamageable damageable = collision.GetComponentInChildren<IDamageable>();
+        if (damageable == null)
+            return;
+        damageData.target = collision.gameObject;
+        damageable.TakeDamage(damageData);
+        cachedHitColliders.Add(collision);
+    }
 
-        // Get all colliders within the box area
-        Collider2D[] colliders = Physics2D.OverlapBoxAll(adjustedBoxCenter, boxSize, boxAngle, layerMask);
+    private bool IsFriendlyTarget(Collider2D collision)
+    {
+        if (owningPlayerPawn && collision.GetComponent<PlayerPawn>())
+            return true;
+        if (owningEnemyPawn && collision.GetComponent<EnemyPawn>())
+            return true;
+        return false;
+    }
 
-        // Process the colliders
-        foreach (Collider2D collider in colliders)
+    private void DamageOverlappingTargets()
+    {
+        if (!hitBoxCollider2D)
+            return;
+
+        List<Collider2D> colliders = new();
+        ContactFilter2D contactFilter = new()
         {
-            // Perform your logic here with each collider
-            Debug.Log("Picked object: " + collider.gameObject.name);
-        }
-    }*/
+            useLayerMask = true,
+            layerMask = affectedLayerMask
+        };
+        Physics2D.OverlapCollider(hitBoxCollider2D, contactFilter, colliders);
+
+        foreach (Collider2D collider in colliders)
+            DamageCollider(collider);
+    }
 }
